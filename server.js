391cb2e23ba40a71b623ba725e36c312e1894984
James C Outland <crawfordoutland12@gmail.com>
2026-02-03 20:48:31 -0500

    Update Meta Pixel ID

--- server.js
+++ server.js
@@ -597,7 +597,23 @@ function renderPageHTML({ title, description, canonical, breadcrumbItems, body, 
     <meta name="description" content="${description}" />
     <link rel="canonical" href="${canonical}" />
     <link rel="stylesheet" href="/styles-modern.css" />
-    <!-- Meta Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f.fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '4311998169024976'); fbq('track', 'PageView');</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=4311998169024976&ev=PageView&noscript=1"/></noscript><!-- End Meta Pixel Code -->
+    <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1447731666875537');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1447731666875537&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
     <script type="application/ld+json">${JSON.stringify(jsonLd)}</script>
   </head>
   <body class="seo-page">
